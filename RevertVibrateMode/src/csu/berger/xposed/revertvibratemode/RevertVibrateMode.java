7a42c28164d779e398242c04c7dd3e01dcfb91bb
Shimon Berger <jadeboy7@gmail.com>
2014-04-28 13:40:11 -0400

    fix name in xposed_init fix description in manifest include latest xposed jar add proper method hook name add comments with info

--- RevertVibrateMode/src/csu/berger/xposed/revertvibratemode/RevertVibrateMode.java
+++ RevertVibrateMode/src/csu/berger/xposed/revertvibratemode/RevertVibrateMode.java
@@ -1,14 +1,33 @@
 package csu.berger.xposed.revertvibratemode;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class RevertVibrateMode implements IXposedHookLoadPackage{
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+//        XposedBridge.log("Loaded app: " + lpparam.packageName);
+		
+		// check for package?
+		
+		findAndHookMethod("com.android.server.NotificationManagerService", lpparam.classLoader, "enqueueNotificationInternal", new XC_MethodHook() {
+			@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				
+				/*
+				 * need to cancel out "final boolean convertSoundToVibration"
+				 * 
+				 * line 1904 in 4.4.2 r1 - latest (http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/4.4.2_r1/com/android/server/NotificationManagerService.java/)
+				 * 
+				 * line 1091 in 4.2 r1 - original change (http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/4.2_r1/com/android/server/NotificationManagerService.java/)
+				*/
+				
+            }
+		});
+		
 	}
 
 }
